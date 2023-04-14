81d1c296ab796f884b163693a4561aa5ecf83936
Kadu Viana <kadu_viana10@hotmail.com>
2023-04-14 09:13:08 -0300

    a

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
 import express from 'express';
-import { pool } from './mysql';
-import { v4 as uuidv4 } from 'uuid';
 import { config } from 'dotenv';
-import { hash, compare } from 'bcrypt';
-import { sign } from 'jsonwebtoken';
 import { userRoutes } from './routes/user.routes';
 import { videosRoutes } from './routes/videos.routes';
 
